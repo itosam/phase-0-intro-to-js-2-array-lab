d83e0a165d6ec542a5fdaae66663eca46aa697a4
Osamu Ito <mr.samito@gmail.com>
2022-09-02 22:52:59 -0400

    commit tree 717208f246aee7e9e1df1a56822146094b850da6

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(name) {
+  return cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+  return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+  return cats.shift();
+}
+
+function appendCat(name){
+  const newCats = [...cats, name];
+  return newCats;
+}
+
+function prependCat(name){
+  const catsTwo = [name, ...cats];
+  return catsTwo
+}
+
+function removeFirstCat() {
+  return cats.slice(1)
+}
+
+function removeLastCat() {
+ return cats.slice(0, cats.length - 1);
+}
